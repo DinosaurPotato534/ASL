fe91125bbac72393f64356fe20ba843cd8bac987
DinosaurPotato534 <shaany0418@gmail.com>
2023-10-21 19:19:26 -0400

    everythings brokennnnnnn

--- Site/ASL/src/components/ASLVideo.jsx
+++ Site/ASL/src/components/ASLVideo.jsx
@@ -5,28 +5,56 @@ import * as tf from '@tensorflow/tfjs';
 function ASLVideo() {
   const webcamRef = useRef(null);
   const videoConstraints = {
-    width: 1280,
-    height: 720,
+    width: 224,
+    height: 224,
     facingMode: 'user',
   };
 
   useEffect(() => {
     const loadModel = async () => {
-      const model = await tf.loadLayersModel('/english_model_js/model.json');
+      try {
+        const model = await tf.loadLayersModel('/english_model_js/model.json');
 
-      // Access the webcam and process video frames
-      const webcam = webcamRef.current.video;
-      const video = tf.browser.fromPixels(webcam);
+        async function predictFrame() {
+          try {
+            // Access the webcam and process video frames
+            const webcam = webcamRef.current.video;
+            const frame = tf.browser.fromPixels(webcam);
 
-      async function predictFrame() {
-        const prediction = model.predict(video);
-        // Log the prediction to the console
-        console.log('Model Prediction:', prediction.dataSync());
-        prediction.dispose(); // Dispose to avoid memory leaks
-        requestAnimationFrame(predictFrame);
+            // Resize the captured frame to [224, 224]
+            const resizedFrame = tf.image.resizeBilinear(frame, [224, 224]);
+
+            // Ensure that the frame has 3 color channels
+            const frameWithRGB = tf.tidy(() => {
+              if (resizedFrame.shape[2] === 3) {
+                return resizedFrame;
+              }
+              return tf.concat([resizedFrame, resizedFrame, resizedFrame], 2);
+            });
+
+            // Perform predictions using the model
+            const prediction = model.predict(frameWithRGB);
+
+            // Log the prediction to the console
+            console.log('Model Prediction:', prediction.dataSync());
+
+            // Dispose of tensors to avoid memory leaks
+            frame.dispose();
+            resizedFrame.dispose();
+            frameWithRGB.dispose();
+            prediction.dispose();
+          } catch (error) {
+            console.error('Error in predictFrame:', error);
+          }
+
+          // Request the next frame
+          requestAnimationFrame(predictFrame);
+        }
+
+        predictFrame();
+      } catch (error) {
+        console.error('Error loading the model:', error);
       }
-
-      predictFrame();
     };
 
     loadModel();
