b99e7f5b043d0adff0858a3b75ef18999cafe187
Rose <prosado3@gatech.edu>
2023-10-22 04:34:05 -0400

    Fixed buttons from page to page

--- Site/ASL/src/main.jsx
+++ Site/ASL/src/main.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from './components/HomePage'
 import VideoPage from './components/VideoPage'
-<<<<<<< Updated upstream
-import ASLVideo from './components/ASLVideo'
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.js"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VideoPage />
-  </React.StrictMode>,
-=======
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -23,5 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </Switch>
     </BrowserRouter>
   </React.StrictMode>
->>>>>>> Stashed changes
 );
