b66ec476b5cb479421e0c79cd98f81ccf95550fc
Rose <prosado3@gatech.edu>
2023-10-22 04:31:17 -0400

    Fixed buttons from page to page

--- Site/ASL/src/components/HomePage.jsx
+++ Site/ASL/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import '../styles/HomePage.css';
 
 const HomePage = () => {
@@ -9,6 +10,27 @@ const HomePage = () => {
 
       <div className='orange'>
         <h1 className='title'>Sign Language Detector</h1>
+=======
+import "../styles/HomePage.css"
+import sideImage from "../images/side-image.png"
+import logo from "../images/logo.png"
+import caret from "../images/lang-caret.png"
+import {Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    
+    <div className="desktop-color">
+      <img className="sign" alt="logo" src={logo} />
+      <Link to="/video"><button className="start-button">
+        <div className="overlap-group">
+          <div className="text-wrapper">Start</div>
+        </div>
+      </button></Link>
+      <div className="languages-button">
+        <div className="div">Languages</div>
+        <img className="languages-caret" alt="Languages caret" src={caret} />
+>>>>>>> Stashed changes
       </div>
       <div className='d-flex justify-content-center align-items-center image-box'>
       <img src='/side-image.png' className='image'/>
--- Site/ASL/src/main.jsx
+++ Site/ASL/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from './components/HomePage'
 import VideoPage from './components/VideoPage'
+<<<<<<< Updated upstream
 import ASLVideo from './components/ASLVideo'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.js"
@@ -10,4 +11,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <VideoPage />
   </React.StrictMode>,
+=======
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+       <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/video" component={VideoPage} />
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>
+>>>>>>> Stashed changes
 );
